ad10898f56fa09ec1ea63c0166dca108b5b4dfcc
peng.he <2236117954@qq.com>
2026-01-05 15:38:21 +0800

    feat: 使用 package.json 中的名称作为 HTML 标题

--- rsbuild.config.ts
+++ rsbuild.config.ts
@@ -1,12 +1,16 @@
 import { defineConfig } from '@rsbuild/core'
 import { pluginReact } from '@rsbuild/plugin-react'
 // Docs: https://rsbuild.rs/config/
+import pkg from './package.json'
 export default defineConfig({
   plugins: [pluginReact()],
   resolve: {
     alias: {
       '@': './src',
     },
+  },
+  html: {
+    title: pkg.name,
   },
   output: {
     assetPrefix:
